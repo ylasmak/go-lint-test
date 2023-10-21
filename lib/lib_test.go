09d424f0200a9ae464d34483fe56117ed497acdf
Youness Lasmak <youness@Younesss-MacBook-Pro.local>
2023-10-21 10:18:46 -0400

    go test

---
+++ lib/lib_test.go
@@ -0,0 +1,15 @@
+package lib
+
+import (
+	"testing"
+)
+
+func TestGetPrivateIPAddress(t *testing.T) {
+	addrs, err := GetPrivateIPAddress()
+	if err != nil {
+		t.Fail()
+	}
+	if len(addrs) > 0 {
+		t.Error("Error")
+	}
+}
